15c3e73005b7b7c80e2c7cc3703ac2b48b82de10
Seth Levine <slevine@manh.com>
2020-01-04 17:10:02 -0500

    Word Bank

---
+++ src/main/java/com/seth/amazon/WordBank.java
@@ -0,0 +1,9 @@
+package com.seth.amazon;
+
+/*
+You are given a string bank. This bank contains pieces and compounds. Print he pieces that make up compounds in any order. No duplicates.
+An example could be (Super, Highway, SuperHighway). There is only one answer there so the answer would be [Super, Highway].
+But ultimately the answer would be a list of lists. List of possible solutions being a list of words.
+ */
+public class WordBank {
+}
